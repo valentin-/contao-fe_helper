3c0622ac0832c536e67f65d8579828ed4ef6d6e2
Valentin Sampl <valentin.sampl@gmail.com>
2015-03-15 21:01:36 +0100

    delete files

---
+++ languages/de/tl_user.php
@@ -0,0 +1,4 @@
+<?php 
+
+$GLOBALS['TL_LANG']['tl_user']['fe_helper_legend'] = 'Frontend Helper';
+$GLOBALS['TL_LANG']['tl_user']['use_fe_helper'] = array('Frontend Helper aktivieren','Wird nur für Admins freigeschaltet.');
